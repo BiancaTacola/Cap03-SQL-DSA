8d79268324862a602d9cbfe9ec2b27316e456554
Bianca Tacola <95148673+BiancaTacola@users.noreply.github.com>
2023-07-13 14:23:32 -0300

    CONCAT
    A concatenação é uma operação que une duas ou mais strings em uma única string. No contexto da consulta SQL mencionada, a concatenação é utilizada para unir os valores das colunas "inv_nodes" e "quadrante" em uma única coluna chamada "posição_tumor".

---
+++ tb_dados3.sql
@@ -0,0 +1,15 @@
+SELECT * FROM cap03.tb_dados3;
+
+CREATE TABLE cap03.tb_dados4
+AS
+SELECT
+classe,
+idade,
+menopausa,
+tamanho_tumor,
+CONCAT(inv_nodes, '-', quadrante) AS posição_tumor,
+node_caps,
+deg_malig,
+seio,
+irradiando
+FROM cap03.tb_dados3;
